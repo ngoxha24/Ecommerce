61a45f4dc2256ddd38b6c7e9fc866d32acae0d3f
Nguyễn Tùng Dương <duongnt@cystack.net>
2024-05-10 08:35:58 +0700

    update AccountViewModels

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -58,7 +58,7 @@ namespace Ecommerce.Models
         [Display(Name = "Password")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Forgotten password?")]
         public bool RememberMe { get; set; }
     }
 
