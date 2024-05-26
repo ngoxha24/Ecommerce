f2f5717292e03abe010603caa28112220cfaabf7
GiaoLe <giaolexuan104@gmail.com>
2024-05-26 15:51:31 +0700

    add home and shop view

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        [Route("home")]
+        public ActionResult Home()
+        {
+            return View();
+        }
+        [Route("info")]
+        public ActionResult Info()
+        {
+            return View();
+        }
+        [Route("shopping")]
+        public ActionResult Shopping()
+        {
+            return View();
+        }
+        [Route("my-shop")]
+        public ActionResult myShop()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/shopController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class shopController : Controller
+    {
+        // GET: shop
+        [Route("shop")]
+        public ActionResult Shop(int id)
+        {
+            ViewBag.ShopId = id;
+            return View();
+        }
+    }
+}
