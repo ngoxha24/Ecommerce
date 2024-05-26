7e6e385ce32ab2d412bc746243f848fd4d7e6c97
tungduong03 <tungduong512003@gmail.com>
2024-05-26 15:32:55 +0700

    add client assets

---
+++ Assets/clientService.js
@@ -0,0 +1,27 @@
+﻿let app = angular.module("myApp", [])
+app.service("clientService", function ($http) {
+    this.getAllType = function () {
+        let res = $http({
+            method: 'get',
+            url: '/item/listtype',
+            params: {}
+
+        });
+        return res;
+    }
+    this.getDiscountList = function () {
+        let res = $http({
+            method: 'get',
+            url: 'item/discount',
+            params: {},
+        });
+        return res;
+    }
+    this.getItemList = function () {
+        let res = $http({
+            method: 'get',
+            url: 'item/getlist',
+        });
+        return res;
+    }
+});
