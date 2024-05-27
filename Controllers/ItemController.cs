f6ff57eb3118fd929a479f82361759b872389df1
gioitrongxuan <gioi.trongxuan@gmail.com>
2024-05-27 20:06:58 +0700

    add: OrderController and ItemContoller.cs

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: item
+        [Route("product")]
+        public ActionResult Product(int id)
+        {
+            ViewBag.ProductId = id;
+            return View();
+        }
+    }
+}
