c2db739a33d011568f9900487db5a19b37c8c62e
gioitrongxuan <gioi.trongxuan@gmail.com>
2024-05-30 21:36:23 +0700

    modify:cartController

--- Assets/cartController.js
+++ Assets/cartController.js
@@ -1,1 +1,37 @@
-﻿
+﻿//cart controller
+app.controller("cartController", function ($scope, cartService) {
+    $scope.cart = [];
+    $scope.total = 0;
+    $scope.totalItem = 0;
+    $scope.cart = cartService.getCart();
+    $scope.total = cartService.getTotal();
+    $scope.totalItem = cartService.getTotalItem();
+    console.log($scope.cart);
+    $scope.removeItem = function (index) {
+        cartService.removeItem(index);
+        $scope.cart = cartService.getCart();
+        $scope.total = cartService.getTotal();
+        $scope.totalItem = cartService.getTotalItem();
+    }
+    $scope.updateItem = function (index, quantity) {
+        cartService.updateItem(index, quantity);
+        $scope.cart = cartService.getCart();
+        $scope.total = cartService.getTotal();
+        $scope.totalItem = cartService.getTotalItem();
+    }
+    $scope.checkout = function () {
+        cartService.checkout();
+        $scope.cart = cartService.getCart();
+        $scope.total = cartService.getTotal();
+        $scope.totalItem = cartService.getTotalItem();
+    }
+}
+)
+// Path: Assets/clientController.js
+// Compare this snippet from Assets/clientController.js:
+// // homeController
+//
+// app.controller("homeController", function ($scope, clientService) {
+//     clientService.getAllType().then(function (res) {
+//         $scope.listOfType = res.data;
+// 
