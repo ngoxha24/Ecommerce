058a0f761cbc965bc195bc1a2cf5581332f82177
gioitrongxuan <gioi.trongxuan@gmail.com>
2024-06-02 22:57:48 +0700

    modify: update itemcontroller, add to card, add to wishlist button. View product

--- Assets/itemController.js
+++ Assets/itemController.js
@@ -24,5 +24,40 @@
         console.log(index);
         $scope.currentCate = index;
     }
+
+    // This function is called when the user clicks the "Add to Cart" button G
+    $scope.addToCart = function (item) {
+        itemService.addToCart(item);
+    }
+    $scope.buyNow = function (item) {
+        itemService.buyNow(item);
+    }
+    $scope.changeQuantity = function (item) {
+        itemService.changeQuantity(item);
+    }
+    $scope.removeItem = function (item) {
+        itemService.removeItem(item);
+    }
+    $scope.getCart = function () {
+        itemService.getCart();
+    }
+    $scope.checkout = function () {
+        itemService.checkout();
+    }
+
+    $scope.getCart();
+
+    // This function is called when the user clicks the "Add to Wishlist" button
+    $scope.addToWishlist = function (item) {
+        itemService.addToWishlist(item);
+    }
+    $scope.getWishlist = function () {
+        itemService.getWishlist();
+    }
+    $scope.getWishlist();
+    $scope.removeFromWishlist = function (item) {
+        itemService.removeFromWishlist(item);
+    }
+    $scope.getWishlist();
     
 })
